41a06f4ea03d40ee5e963113ec45d97abb9a0282
millerk <mkm2010@gmail.com>
2024-08-13 20:46:47 -0700

    Fixing Tile Layer Collisions
    1. Adding new layers to tiles to distinguish types.
    2. Updated collision matrix in project to reflect behavior "ships can't move onto land, ship can shoot across sand, but cannot shoot across grass".
    3. Updated Cannoball logic to destroy itself on collision with grass as expected.
    4. Updated Battlefield scene to include LevelManager from previous commit (I did not hit save on scene view before pushing lol)

--- Sparrow/Assets/Scripts/Cannons/CannonballLogic.cs
+++ Sparrow/Assets/Scripts/Cannons/CannonballLogic.cs
@@ -8,6 +8,10 @@ public class CannonballLogic : MonoBehaviour
     public Vector3 projectileSpawnPoint;
     public float projectileSpeed;
 
+    // From inspector
+    private int _enemyLayer = 8;
+    private int _grassLayer = 10;
+
     void Start()
     {
         projectileSpawnPoint = transform.position;
@@ -24,8 +28,11 @@ public class CannonballLogic : MonoBehaviour
     }
 
     void OnCollisionEnter2D(Collision2D other){
-        // Check if we hit something (land, enemy) that would cause us to explode
-        if (other.gameObject.CompareTag("Enemy"))
+        // Check if we hit something (land, enemy) that would cause us to "explode"
+        // Can combine into a LayerMask check but keeping separate in case we want to
+        // do something different based on the collision source
+        if (other.gameObject.layer == _enemyLayer ||
+            other.gameObject.layer == _grassLayer)
         {
             Destroy(gameObject);
         }
