0fd3e205e882f02553607e1cdd20e49defa0d13e
millerk <mkm2010@gmail.com>
2024-08-19 19:17:58 -0700

    Refactoring Health Manager
    1. Refactored EnemyHealthManager to a system reusable by Player object.
    2. Added basic "bump into enemy" damage event to player ship.
    3. Added player health events to PlayerStatus.
    4.  Added basic UI for player health.

--- Sparrow/Assets/Scripts/GameState/PlayerStatus.cs
+++ Sparrow/Assets/Scripts/GameState/PlayerStatus.cs
@@ -5,9 +5,16 @@ public class PlayerStatus : MonoBehaviour
 {
     public int totalGold = 0;
     public Text currencyText;
+    public Text healthText;
     public void OnGoldCollection(GameObject gold)
     {
         totalGold += gold.GetComponent<Gold>().value;
         currencyText.text = totalGold.ToString();
+    }
+
+    public void OnHealthUpdate(GameObject player)
+    {
+        int health = player.GetComponent<HealthManager>().HitPoints;
+        healthText.text = health.ToString();
     }
 }
