af34078ec4ffa7e4c9d15f51b5c474d1478366a8
Bryan Yee <bryanmasayee@gmail.com>
2024-09-02 23:03:52 -0700

    Revert "Merge pull request #37 from millerk/cole-2.0"
    This reverts commit e26666aad760b238decedef0c235e322dc970edc, reversing
    changes made to e8e774058342e057456fa3b573b633bfe048885b.

--- Sparrow/Assets/Scripts/Player/ShipMovement.cs
+++ Sparrow/Assets/Scripts/Player/ShipMovement.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum MovementMode
+
+public enum MovementMode 
 {
     mouseLook,
     tank
@@ -11,20 +13,16 @@ public enum MovementMode
 public class ShipMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    public float dashSpeed = 10f; // Speed during dash
-    public float dashDuration = 0.2f; // Duration of the dash
-    public float dashCooldown = 1f; // Cooldown between dashes
 
     public Rigidbody2D rb;
     public Camera cam;
     public MovementMode moveMode;
     public float rotateSpeed;
 
-    private Vector2 movement;
-    private Vector2 mousePos;
-    private float angle;
-    private bool isDashing = false;
-    private float lastDashTime = -Mathf.Infinity;
+
+    Vector2 movement;
+    Vector2 mousePos;
+    float angle;
 
     void Start()
     {
@@ -34,32 +32,21 @@ public class ShipMovement : MonoBehaviour
             Debug.LogError("Main camera is either not found or not perspective. Ensure there's a perspective camera with the 'MainCamera' tag.");
         }
     }
-
+    // Update is called once per frame
     void Update()
     {
         if (moveMode == MovementMode.mouseLook)
         {
             updateMovementMouseLook();
-        }
-        else
-        {
+        } else {
             // default to tank
             updateMovementTank();
-        }
-
-        // Dash functionality
-        if (Input.GetKeyDown(KeyCode.Space) && Time.time >= lastDashTime + dashCooldown)
-        {
-            StartCoroutine(Dash());
         }
     }
 
     void FixedUpdate()
     {
-        if (!isDashing)
-        {
-            rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
-        }
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
         rb.rotation = angle - 90f;
     }
 
@@ -80,27 +67,9 @@ public class ShipMovement : MonoBehaviour
         angle -= Input.GetAxisRaw("Horizontal") * rotateSpeed * 360 * Time.deltaTime;
         // Keep within range of [-360, 360] for sanity's sake
         angle %= 360;
-
+        
         float angleRad = angle * Mathf.Deg2Rad;
         movement.x = Mathf.Cos(angleRad) * moveDir;
         movement.y = Mathf.Sin(angleRad) * moveDir;
-    }
-
-    private IEnumerator Dash()
-    {
-        isDashing = true;
-        lastDashTime = Time.time;
-        Vector2 dashDirection = movement.normalized;
-
-        // Perform the dash
-        float startTime = Time.time;
-        while (Time.time < startTime + dashDuration)
-        {
-            rb.MovePosition(rb.position + dashDirection * dashSpeed * Time.fixedDeltaTime);
-            yield return null;
-        }
-
-        // End the dash
-        isDashing = false;
     }
 }
