f52db1de5a6230b92c0a82d819f8805123ddae4e
jerobbs <robbinsfour@gmail.com>
2024-09-08 23:39:38 -0700

    Intro
    This is the first cutscene

---
+++ Sparrow/Assets/Art/Ui Ux/Introdial.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public Text dialogueText; // Reference to the Text component
+    public string[] dialogues; // Array of dialogues
+    private int currentDialogueIndex = 0; // Current index in the dialogue array
+
+    void Start()
+    {
+        if (dialogues.Length > 0)
+        {
+            dialogueText.text = dialogues[currentDialogueIndex];
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // Detect left mouse button click
+        {
+            AdvanceDialogue();
+        }
+    }
+
+    void AdvanceDialogue()
+    {
+        if (currentDialogueIndex < dialogues.Length - 1)
+        {
+            currentDialogueIndex++;
+            dialogueText.text = dialogues[currentDialogueIndex];
+        }
+        else
+        {
+            ;
+        }
+    }
+}
